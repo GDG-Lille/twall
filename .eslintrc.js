be5349fa86613a345458af08149e930ff5cebb63
François GRUCHALA <francois@gruchala.me>
2019-05-12 17:28:33 +0200

    :rotating_light: make eslint happy

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'arrow-parens': 'off',
     'no-param-reassign': 'warn',
     'class-methods-use-this': 'warn',
+    'no-underscore-dangle': 'warn',
   },
   parserOptions: {
     parser: 'babel-eslint',
